8495caac472342d1203b5e669eb97b0d1d3ded0f
hannahellis4242 <hannahellis4242@gmail.com>
2023-10-10 08:26:03 +0100

    Adding scripts and trying a run in main

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,12 @@ import { showSimplex, step } from "./simplex/Two/Simplex";
 
 const fn = ({ x, y }: Point2D) => x * x + y * y;
 const stepFn = step(fn);
-const result = new Array(10).fill(0).reduce(
+const result = new Array(100).fill(0).reduce(
   (last, _) => {
     const next = stepFn(last);
     console.log(showSimplex(last));
     return next;
   },
-  [createPoint2D(2, 6), createPoint2D(8, 12), createPoint2D(8, 6)]
+  [createPoint2D(20, 60), createPoint2D(80, 120), createPoint2D(80, 60)]
 );
 console.log(showSimplex(result));
